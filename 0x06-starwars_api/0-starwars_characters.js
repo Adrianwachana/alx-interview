ae5bf1e6e62f694ef075f0396c68aa4508b60bda
Adrianwachana <adrianwafula403@gmail.com>
2024-01-06 10:36:05 +0300

    test

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,52 +1,28 @@
 #!/usr/bin/node
-// script that prints all the characters of a Star Wars movie:
 
 const request = require('request');
 
-const getCharacterName = (characterUrl) => {
-  return new Promise((resolve, reject) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body).name);
-      }
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
+
+// Makes an API request to get film information
+request(filmURL + filmNum, async function (err, res, body) {
+  if (err) return console.error(err);
+
+  // Parse the response body to get the list of character URLs
+  const charURLList = JSON.parse(body).characters;
+
+  // Iterare through the character URLs and fect character information
+  // Make a request to each character URL
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) return console.error(err);
+
+        // Parse the charcter nformation and print the character's name Resolve the promise to indicate completion
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
-};
-
-const getMovieCharacters = (movieId) => {
-  return new Promise((resolve, reject) => {
-    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const characters = JSON.parse(body).characters;
-        resolve(characters);
-      }
-    });
-  });
-};
-
-const printCharacters = async (movieId) => {
-  try {
-    const characters = await getMovieCharacters(movieId);
-
-    for (const character of characters) {
-      const characterName = await getCharacterName(character);
-      console.log(characterName);
-    }
-  } catch (error) {
-    console.error(error);
   }
-};
-
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Please provide a Movie ID as a command line argument.');
-} else {
-  printCharacters(movieId);
-}
+});
